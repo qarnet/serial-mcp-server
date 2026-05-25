0c574a54f6c955882d652b5bddf87a5fd70a2961
Thomas Kirschner <thomaskirschner85@yahoo.de>
2026-05-25 04:20:00 +0200

    refactor: rename handler module to server

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 pub mod codec;
 pub mod error;
-pub mod handler;
 pub mod prompts;
 pub mod resources;
 pub mod security;
 pub mod serial;
+pub mod server;
 pub mod tools;
 
 pub use error::{Result, SerialError};
-pub use handler::SerialHandler;
+pub use server::SerialHandler;
